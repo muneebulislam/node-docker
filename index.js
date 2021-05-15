c4d606a4ba84061ff2d26ef3ee3fac597a040815
Muneeb Ul Islam <mui850@usask.ca>
2021-05-15 15:24:21 -0600

    added mongo database in docker-compose.yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req,res)=> {
-    res.send("Hello World!")
+    res.send("Hello World. I am here!")
 })
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listerning on port ${port}` ));
